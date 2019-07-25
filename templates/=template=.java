6b0b448778715300976b535c2804dd169a796e68
icgw <icgw@outlook.com>
2019-07-25 11:20:03 +0800

    Add java template

---
+++ templates/=template=.java
@@ -0,0 +1,17 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+package %HERE%;
+
+public class %FILE%
+{
+  public %FILE%() {
+  }
+
+  public static void main(String[] args) {
+  }
+}
